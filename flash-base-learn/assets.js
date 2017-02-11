0c4c7db7f5026bf6199f33ab9468cfb82c24496a
GodWei <x-weize@foxmail.com>
2017-02-11 17:10:00 +0800

    提交修改

--- flash-base-learn/assets.js
+++ flash-base-learn/assets.js
@@ -9,16 +9,16 @@ lib.properties = {
 	fps: 24,
 	color: "#FFFFFF",
 	manifest: [
-		{src:"images/_01_0000_logo01.png?1486748856687", id:"_01_0000_logo01"},
-		{src:"images/_01_0000_text01_01.png?1486748856687", id:"_01_0000_text01_01"},
-		{src:"images/_01_0001_rocket.png?1486748856687", id:"_01_0001_rocket"},
-		{src:"images/_01_0001_text01_02.png?1486748856687", id:"_01_0001_text01_02"},
-		{src:"images/_01_0002_rocketfire.png?1486748856687", id:"_01_0002_rocketfire"},
-		{src:"images/_01_0002_text01_03.png?1486748856687", id:"_01_0002_text01_03"},
-		{src:"images/_01_0003_DREAM.png?1486748856687", id:"_01_0003_DREAM"},
-		{src:"images/_01_0005_plane.png?1486748856687", id:"_01_0005_plane"},
-		{src:"images/_01_0006_BJ.jpg?1486748856687", id:"_01_0006_BJ"},
-		{src:"images/arrow.png?1486748856687", id:"arrow"}
+		{src:"images/_01_0000_logo01.png?1486801432078", id:"_01_0000_logo01"},
+		{src:"images/_01_0000_text01_01.png?1486801432078", id:"_01_0000_text01_01"},
+		{src:"images/_01_0001_rocket.png?1486801432078", id:"_01_0001_rocket"},
+		{src:"images/_01_0001_text01_02.png?1486801432078", id:"_01_0001_text01_02"},
+		{src:"images/_01_0002_rocketfire.png?1486801432078", id:"_01_0002_rocketfire"},
+		{src:"images/_01_0002_text01_03.png?1486801432078", id:"_01_0002_text01_03"},
+		{src:"images/_01_0003_DREAM.png?1486801432078", id:"_01_0003_DREAM"},
+		{src:"images/_01_0005_plane.png?1486801432078", id:"_01_0005_plane"},
+		{src:"images/_01_0006_BJ.jpg?1486801432078", id:"_01_0006_BJ"},
+		{src:"images/arrow.png?1486801432078", id:"arrow"}
 	]
 };
 
@@ -267,28 +267,130 @@ p.nominalBounds = new cjs.Rectangle(-53,0,267,275);
 p.nominalBounds = new cjs.Rectangle(-49,-49,98,98);
 
 
-(lib.view1 = function(mode,startPosition,loop) {
+(lib.view2 = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
 	// timeline functions:
-	this.frame_0 = function() {
-		var _this = this;
-		_this.stop();
-		_this.btn.addEventListener("click",function (){
-		           alert("play");
-		      _this.play();
-		      })
-	}
 	this.frame_45 = function() {
 		if(model) model.dispatchEvent("textready");
 	}
 	this.frame_116 = function() {
 		this.stop();
-		if(model) model.dispatchEvent("complete");
+		this.addEventListener("click",function(){
+				if(model) model.dispatchEvent("completeClick2");
+		})
 	}
 
 	// actions tween:
-	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(45).call(this.frame_45).wait(71).call(this.frame_116).wait(86));
+	this.timeline.addTween(cjs.Tween.get(this).wait(45).call(this.frame_45).wait(71).call(this.frame_116).wait(86));
+
+	// 图层 2
+	this.btn = new lib.mc1();
+	this.btn.setTransform(375,603);
+
+	this.timeline.addTween(cjs.Tween.get(this.btn).wait(202));
+
+	// jiantou
+	this.instance = new lib.arrow_1();
+	this.instance.setTransform(375,1106.9,0.5,0.5);
+	this.instance.alpha = 0.012;
+	this.instance._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(70).to({_off:false},0).to({scaleX:1,scaleY:1,alpha:1},15,cjs.Ease.get(1)).wait(117));
+
+	// text01_01
+	this.instance_1 = new lib.text01_01();
+	this.instance_1.setTransform(371.9,596,1,1,0,0,0,276,45);
+	this.instance_1.alpha = 0.012;
+	this.instance_1._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(46).to({_off:false},0).to({y:628,alpha:1},36,cjs.Ease.get(1)).to({_off:true},106).wait(14));
+
+	// text_01_02
+	this.instance_2 = new lib.text_01_02();
+	this.instance_2.setTransform(371.9,710,1,1,0,0,0,277,45);
+	this.instance_2.alpha = 0.012;
+	this.instance_2._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(49).to({_off:false},0).to({y:742,alpha:1},36,cjs.Ease.get(1)).to({_off:true},103).wait(14));
+
+	// text01_03
+	this.instance_3 = new lib.text01_03();
+	this.instance_3.setTransform(371.4,849.9,1,1,0,0,0,277.5,73);
+	this.instance_3.alpha = 0.012;
+	this.instance_3._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(52).to({_off:false},0).to({y:881.9,alpha:1},36,cjs.Ease.get(1)).to({_off:true},100).wait(14));
+
+	// text01_04
+	this.instance_4 = new lib.text01_04();
+	this.instance_4.setTransform(57.9,770.1,0.118,1,0,0,0,0,227.1);
+	this.instance_4.alpha = 0.012;
+	this.instance_4._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(26).to({_off:false},0).to({scaleX:1,alpha:1},26,cjs.Ease.get(1)).to({_off:true},136).wait(14));
+
+	// logo
+	this.instance_5 = new lib.logo();
+	this.instance_5.setTransform(364.1,9.2,1,1,0,0,0,237,167.5);
+	this.instance_5.alpha = 0.012;
+	this.instance_5._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(2).to({_off:false},0).to({y:347.2,alpha:1},18,cjs.Ease.get(1)).to({_off:true},168).wait(14));
+
+	// dream
+	this.instance_6 = new lib.dream();
+	this.instance_6.setTransform(320.1,317.7,1,1,0,0,0,170,197);
+	this.instance_6._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(1).to({_off:false},0).wait(13).to({_off:true},158).wait(30));
+
+	// rocketZ
+	this.instance_7 = new lib.rocketZ();
+	this.instance_7.setTransform(-135.5,804.8,1,1,0,0,0,79.5,137);
+	this.instance_7.alpha = 0.98;
+	this.instance_7._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(2).to({_off:false},0).to({x:532.1,y:268.7},18,cjs.Ease.get(1)).to({_off:true},168).wait(14));
+
+	// plane
+	this.instance_8 = new lib.plane();
+	this.instance_8.setTransform(533.3,1053.2,0.93,0.93,0,0,0,233,207);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_8).to({_off:true},188).wait(14));
+
+	// BJ
+	this.instance_9 = new lib.BJ();
+	this.instance_9.setTransform(375,603,1,0.904,0,0,0,375,667);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_9).to({_off:true},188).wait(14));
+
+	// 图层 1
+	this.btn_1 = new lib.mc1();
+	this.btn_1.setTransform(336.6,409.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.btn_1).to({_off:true},1).wait(201));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-38.4,-193.6,788.5,1439.3);
+
+
+(lib.view1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_45 = function() {
+		if(model) model.dispatchEvent("textready");
+	}
+	this.frame_116 = function() {
+		this.stop();
+		this.addEventListener("click",function(){
+				if(model) model.dispatchEvent("completeClick");
+		})
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(45).call(this.frame_45).wait(71).call(this.frame_116).wait(86));
 
 	// 图层 2
 	this.btn = new lib.mc1();
--- flash-base-learn/test.js
+++ flash-base-learn/test.js
@@ -1,26 +1,54 @@
 /**
  * Created by xiaweize on 2017/2/11.
  */
-var canvas = document.getElementById("mainView"),stage;
+var canvas = document.getElementById("mainView"),stage,viewList;
 var model = new createjs.EventDispatcher();//用来捕获事件的原型
+function resizeCanvas(){
+    model = new createjs.EventDispatcher();//用来捕获事件的原型
+    stageWidth =  document.documentElement.clientWidth;
+    stageHeight = document.documentElement.clientHeight;
+    stageScale = stageWidth/(750/2);
+    canvas = document.getElementById("mainView");
+    if(stageWidth/stageHeight > 0.665)
+    {
+        stageScale = stageHeight/(1206/2);
+        canvas.style.left = (stageWidth - 750/2*stageScale)/2 + 'px';
+    }
+    else
+    {
+        stageScale = stageWidth/(750/2);
+    }
+    canvas.style.width = 750/2*stageScale + 'px';
+    canvas.style.height = 1206/2*stageScale + 'px';
+}
 function init(){
     stage = new createjs.Stage(canvas);
     images = images||{};
     var loader = new createjs.LoadQueue(true);
+
     loader.on("fileload", handleFileLoad);
     loader.on("complete", loadCompleteHandler);
     loader.loadManifest(lib.properties.manifest);
 
     createjs.Ticker.setFPS(lib.properties.fps);
     createjs.Ticker.on("tick", stageBreakHandler);
-    bindFlashEvent();
+
+    resizeCanvas();
 }
 /**
  * 资源加载完毕
  */
 function loadCompleteHandler(){
    view1= new lib.view1();
-   stage.addChild(view1);
+   var view2 = new lib.view2();
+   viewList = [view1,view2];
+   stage.addChild(viewList[0]);
+   stage.addChild(viewList[1]);
+    viewList[0].y=0;
+    viewList[1].y=1206;
+    viewList[0].gotoAndPlay(0);
+    viewList[1].gotoAndStop(0);
+    bindFlashEvent();//事件监听需要在资源加载完毕后绑定
 }
 function handleFileLoad(evt) {
     if (evt.item.type == "image") { images[evt.item.id] = evt.result; }
@@ -30,10 +58,23 @@ function stageBreakHandler(event)
     stage.update();
 }
 function bindFlashEvent(){
-    model.addEventListener("complete",function(){
-        console.log("complete")
+    model.on("completeClick",function(){
+        createjs.Tween.get(viewList[0]).to({y:-1206}, 500);
+        createjs.Tween.get(viewList[1]).to({y:0}, 500).call(function(){
+            viewList[1].gotoAndPlay(0);
+            viewList[0].gotoAndStop(0);
+        });
+
     })
-    model.addEventListener("textready",function(){
-        console.log("textready")
+    model.on("completeClick2",function(){
+        createjs.Tween.get(viewList[0]).to({y:0}, 500).call(function(){
+            viewList[0].gotoAndPlay(0);
+            viewList[1].gotoAndStop(0);
+        });;
+        createjs.Tween.get(viewList[1]).to({y:1206}, 500)
+
+    })
+    model.on("textready",function(){
+        console.log("textready");
     })
 }
